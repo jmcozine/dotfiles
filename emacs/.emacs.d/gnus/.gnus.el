6f1b56c520cc532d3d82c50d30cac5840e9ac138
James Cozine <jmcozine@gmail.com>
2014-11-29 21:28:23 -0500

    Remove gravatar display (was causing issues)

--- emacs/.emacs.d/gnus/.gnus.el
+++ emacs/.emacs.d/gnus/.gnus.el
@@ -4,5 +4,8 @@
       gnus-read-newsrc-file nil
       gnus-save-killed-list nil
       gnus-check-new-newsgroups nil
-      gnus-startup-file "~/.emacs.d/gnus/newsrc")
+      gnus-startup-file "~/.emacs.d/gnus/newsrc"
+      ;gnus-treat-from-gravatar 'head
+      gnus-treat-body-boundary nil)
 
+(setq mm-discouraged-alternatives '("text/html"))
