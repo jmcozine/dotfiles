7d93307f5926dbb9d2565088a0d9ae03063620e4
James Cozine <jmcozine@gmail.com>
2021-09-03 04:18:40 -0500

    fixed botched merge of emacs init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-=======
-;; ~/.emacs.d/init.el
-;; Time-stamp: <2019-02-06 12:19:19 jc>
-
->>>>>>> db4fac6b98ede9eee26c4b6568dfd81a2173f745
 (when window-system
   (menu-bar-mode -1)
   (tool-bar-mode -1)
@@ -24,7 +18,7 @@
 
 (eval-when-compile
   (require 'use-package))
-;(require 'diminish)
+(require 'diminish)
 (require 'bind-key)
 
 ;; themes
@@ -55,7 +49,6 @@
 
 (setq gnutls-min-prime-bits 1024)
 
-<<<<<<< HEAD
 ;; Org-journal
 (use-package org-journal
   :ensure t
@@ -84,96 +77,19 @@
 ;;   (save-buffer)
 ;;   (kill-buffer-and-window))
 ;; (define-key org-journal-mode-map (kbd "C-x C-s") 'org-journal-save-entry-and-exit)
-=======
-;; Golang
-
-(use-package company-go
-  :ensure t
-  :config
-  (setq company-go-gocode-command "/home/jc/bin/gocode"))
-
-(use-package go-mode
-  :ensure t
-;  :init (require 'go-mode-autoloads)
-  :config
-  (setq gofmt-command "goimports")
-  (add-hook 'before-save-hook 'gofmt-before-save)
-  (add-hook 'go-mode-hook (lambda ()
-                            (set (make-local-variable 'company-backends) '(company-go))
-                            (company-mode)
-                            (setq tab-width 4)
-                            (setq indent-tabs-mode 1))))
-
-;; Word Count
-(use-package wc-mode
-  :ensure t)
-
-;; Magit
-(use-package magit
-  :ensure t
-  :bind ("C-x g" . magit-status))
-
-;; Helm
-(use-package helm
-  :ensure t
-  :diminish helm-mode
-  :init (require 'helm-config)
-  :bind (("M-x" . helm-M-x)
-         ("C-x C-f" . helm-find-files)
-         :map helm-map
-         ("<tab>" . helm-execute-persistent-action)
-         ("C-z" . helm-select-action))
-  :config (setq helm-split-window-in-side-p t
-                helm-ff-file-name-history-use-recentf t))
-
-;; Twittering
-(use-package twittering-mode
-  :config (setq twittering-use-master-password t))
->>>>>>> db4fac6b98ede9eee26c4b6568dfd81a2173f745
 
 (add-hook 'org-shiftup-final-hook 'windmove-up)
 (add-hook 'org-shiftleft-final-hook 'windmove-left)
 (add-hook 'org-shiftdown-final-hook 'windmove-down)
 (add-hook 'org-shiftright-final-hook 'windmove-right)
 
-<<<<<<< HEAD
-=======
-;; Org-journal
-;; (use-package org-journal
-;;   :ensure t
-;;   :config (setq org-journal-enable-encryption nil))
-
-;; ledger
-(use-package ledger-mode
-  :ensure t
-  :mode "\\.dat\\'")
-
-;; tramp
-(setq tramp-default-method "ssh")
-(eval-after-load 'tramp '(setenv "SHELL" "/bin/bash"))
-
-(add-to-list 'load-path "/home/jc/.emacs.d/site-lisp")
-(require 'beancount)
-(add-to-list 'auto-mode-alist '("\\.beancount\\'" . beancount-mode))
-
-(setq org-element-use-cache nil)
-
-;; quote region
-(global-set-key (kbd "M-\"") 'insert-pair)
-
->>>>>>> db4fac6b98ede9eee26c4b6568dfd81a2173f745
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-<<<<<<< HEAD
    '(zenburn-theme wc-mode use-package s org-journal magit-popup magit ledger-mode helm diminish company-go)))
-=======
-   (quote
-    (company-jedi org-mode powershell pass helm-pass zenburn-theme wc-mode use-package twittering-mode pinentry org-journal org magit ledger-mode helm-go-package helm-emms flycheck company-go auctex))))
->>>>>>> db4fac6b98ede9eee26c4b6568dfd81a2173f745
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
